88bca00a2c38a7a9ac7676e9149a5a4293facd5c
Rimol chakma <67545874+rimolch@users.noreply.github.com>
2021-05-02 15:35:53 +0600

    Create min_prime.c

---
+++ Max_prime_in_range/min_prime.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main()
+{
+    int t,a,b,c=0,i,j;
+    scanf("%d",&t);
+    while(t--)
+    {
+        scanf("%d%d",&a,&b);
+        for(i=a;i<=b;i++)
+        {
+            c = 0;
+            for(j=2;j*j<=i;j++)
+            {
+                if(i%j==0)
+                {
+                    c = 1;
+                    break;
+                }
+            }
+           if(c==0&&i!=1)
+            {
+                printf("%d\n",i);
+                break;
+            }
+        }
+        if(c==1||a==1||b==1)
+        {
+            printf("-1\n");
+           
+        }
+    }
+    return 0;
+}
